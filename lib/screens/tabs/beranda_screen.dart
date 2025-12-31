18078f9d052d3cb7171e35bd07c927f3a61801a6
MOH. WAKID <wakid.developer@gmail.com>
2025-12-31 16:50:58 +0700

    membuat halaman beranda baru

---
+++ lib/screens/tabs/beranda_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BerandaScreen extends StatelessWidget {
+  const BerandaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("HALAMAN BERANDA",
+          style: TextStyle(color: Colors.white, fontSize: 22)),
+    );
+  }
+}
