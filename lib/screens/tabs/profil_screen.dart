1b23d2ab474421eefc5b4f0718c360c5aeabf0da
MOH. WAKID <wakid.developer@gmail.com>
2025-12-31 16:55:30 +0700

    membuat halaman profil

---
+++ lib/screens/tabs/profil_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfilScreen extends StatelessWidget {
+  const ProfilScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("HALAMAN PROFIL",
+          style: TextStyle(color: Colors.white, fontSize: 22)),
+    );
+  }
+}
