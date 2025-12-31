73fb83879be419f1ceef429ed7a7bb5fc7852ebc
MOH. WAKID <wakid.developer@gmail.com>
2025-12-31 16:53:11 +0700

    membuat halaman kursus

---
+++ lib/screens/tabs/kursus_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KursusScreen extends StatelessWidget {
+  const KursusScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("HALAMAN KURSUS SAYA",
+          style: TextStyle(color: Colors.white, fontSize: 22)),
+    );
+  }
+}
