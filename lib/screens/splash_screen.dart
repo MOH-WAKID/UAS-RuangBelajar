fe8c0e45327ae8ee33055f5816c83cb125a6e717
MOH. WAKID <wakid.developer@gmail.com>
2025-12-31 14:50:54 +0700

    membuat splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(const Duration(milliseconds: 120), (timer) {
+      if (!mounted) return;
+
+      setState(() {
+        progress += 5;
+      });
+
+      if (progress >= 100) {
+        progress = 100;
+        timer.cancel();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0B1220),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon
+            Container(
+              width: 110,
+              height: 110,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.05),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const Icon(
+                Icons.school,
+                color: Colors.white,
+                size: 45,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            const Text(
+              "Ruang Belajar",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              "Solusi cerdas untuk belajar",
+              style: TextStyle(color: Colors.grey),
+            ),
+
+            const SizedBox(height: 60),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "LOADING RESOURCES",
+                      style: TextStyle(
+                        color: Colors.blue[300],
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: LinearProgressIndicator(
+                      minHeight: 8,
+                      value: progress / 100,
+                      color: Colors.blue,
+                      backgroundColor: Colors.white24,
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    "${progress.toInt()}%",
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
